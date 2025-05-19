f795fa8450ef90228a93e1ef45e7fc5f4934a3d2
RogerDuran808 <roger8duran@gmail.com>
2025-05-19 15:33:24 +0200

    modificacio

--- src/ai_health_assistant/01_preprocessing/preprocess_data.py
+++ src/ai_health_assistant/01_preprocessing/preprocess_data.py
@@ -1,5 +1,10 @@
 from ai_health_assistant.utils.prep_helpers import preprocess_data
 
+'''
+Preprocessament de les dades:
+Un cop netejades les dades, les preprocessem, per preparar-les per l'entrenament del model.
+'''
+
 df_prep = preprocess_data(
     input_path="data/df_cleaned.csv",
     output_path="data/df_preprocessed.csv"
