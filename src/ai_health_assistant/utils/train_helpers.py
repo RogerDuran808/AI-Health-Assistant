8d242ed39b5b50428914207f28f0f55ef349e488
RogerDuran808 <roger8duran@gmail.com>
2025-05-20 16:37:06 +0200

    last commit

--- src/ai_health_assistant/utils/train_helpers.py
+++ src/ai_health_assistant/utils/train_helpers.py
@@ -15,3 +15,25 @@ from sklearn.decomposition import PCA
 from imblearn.over_sampling import SMOTE
 from imblearn.pipeline import Pipeline as ImbPipeline
 
+def grid_search(X_train, y_train, model, param_grid, scoring='f1', cv=5):
+    """
+    Perform grid search with cross-validation to find the best hyperparameters for a given model.
+    
+    Parameters:
+    - X_train: Training features
+    - y_train: Training labels
+    - model: The machine learning model to be tuned
+    - param_grid: Dictionary with parameters names (str) as keys and lists of parameter settings to try as values
+    - scoring: Scoring method to evaluate the performance of the model
+    - cv: Number of folds in cross-validation
+    
+    Returns:
+    - best_model: The best model found during the grid search
+    """
+    grid_search = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=cv)
+    grid_search.fit(X_train, y_train)
+    
+    print(f"Best parameters: {grid_search.best_params_}")
+    print(f"Best score: {grid_search.best_score_}")
+    
+    return grid_search.best_estimator_
