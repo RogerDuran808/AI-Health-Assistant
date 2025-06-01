62a7e6606086d41f8354643c536d52eee4f61700
RogerDuran808 <roger8duran@gmail.com>
2025-06-01 17:37:51 +0200

    canvis en el clean

--- src/ai_health_assistant/01_preprocessing/clean_data.py
+++ src/ai_health_assistant/01_preprocessing/clean_data.py
@@ -6,17 +6,48 @@ from ai_health_assistant.utils.clean_helpers import clean_data
 Justificació de la neteja de dades al notebook: 01_LifeSnaps_EDA.ipynb
 L'objectiu es tenir les dades netes i imputades de forma basica per fer un bon preprocessament
 
-Eliminem les columnes irrellevants per la nostre predicció.
+Seleccionem les columnes disponibles del dispositiu fitbit
 Corregim la variable del bmi i afegim categories de bmi_tipo
 Corregim els valors anòmals del dataset
-Fem un drop de columnes segons el EDA realitzat que creiem que no aporten valor a la predicció
 Finalment exportem a CSV
 '''
+TARGET = "TIRED"
 
+FEATURES = [
+        "age",
+        "gender",
+        "bmi",
+        "calories",
+        "steps",
+        "lightly_active_minutes",
+        "moderately_active_minutes",
+        "very_active_minutes",
+        "sedentary_minutes",
+        "resting_hr",
+        "minutes_below_default_zone_1",
+        "minutes_in_default_zone_1",
+        "minutes_in_default_zone_2",
+        "minutes_in_default_zone_3",
+        "minutesToFallAsleep",
+        "minutesAsleep",
+        "minutesAwake",
+        "minutesAfterWakeup",
+        "sleep_efficiency",
+        "sleep_deep_ratio",
+        "sleep_light_ratio",
+        "sleep_rem_ratio",
+        "sleep_wake_ratio",
+        "daily_temperature_variation",
+        "rmssd",
+        "spo2",
+        "full_sleep_breathing_rate",
+    ]
 
 df_clean = clean_data(
     input_path='data/daily_fitbit_sema_df_unprocessed.csv', 
-    output_path='data/df_cleaned.csv'
+    output_path='data/df_cleaned.csv',
+    target=TARGET,
+    features=FEATURES
     )
 
 print(df_clean.info())
